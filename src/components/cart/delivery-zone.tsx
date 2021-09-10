90674e2fcc43599c2720c0f70fa8d428105fefa8
Andrew Phelps <59522519+NOP-slide@users.noreply.github.com>
2021-09-10 10:01:28 +1000

    change time again

--- src/components/cart/delivery-zone.tsx
+++ src/components/cart/delivery-zone.tsx
@@ -56,7 +56,7 @@ function DeliveryZone(): React.ReactElement {
           <p>Delivered on: {deliveryDays}</p>
           <p>
             Please place your order before{' '}
-            {zone === 'Port Macquarie' ? '12pm' : '10am'} the day prior to
+            {zone === 'Port Macquarie' ? '10am' : '10am'} the day prior to
             delivery.
           </p>
         </Button>
